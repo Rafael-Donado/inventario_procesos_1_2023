45f7d9aad60e598d2497205974c0cb20d3e1482c
Rafael Donado <rddonadoc@ufpso.edu.co>
2023-05-02 21:01:32 -0500

    Refactorizacion de mensajes de la api

---
+++ src/main/java/com/procesos/inventario/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.procesos.inventario.utils;
+
+public class Constants {
+    private Constants(){}
+    public static final String REGISTER_CREATED = "Registro creado";
+    public static final String REGISTER_BAD = "Hubo un error al crear el registro ";
+    public static final String REGISTER_NOT_FOUNT = "Registro no encontrado";
+    public static final String REGISTER_UPDATE = "Registro actualizado";
+    public static final String PASSWORD_INCORRECT = "La contraseña es incorrecta";
+    public static final String USER_NOT_FOUNT = "Usuario no encontrado";
+    public static final String USER_LOGIN = "Usuario logeado";
+    public static final String REGISTER_FOUNT = "Registro encontrado";
+    public static final String REGISTER_LIST = "Lista de registros";
+    public static final String ERROR_DATA = "Error en los datos enviados";
+}
